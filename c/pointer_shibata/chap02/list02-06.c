786368af5ed3a380ef4255d96963213631728ceb
Daisuke Yamato <yamato.hep.fermicern@gmail.com>
2015-08-24 07:45:40 +0900

    add list02-07

---
+++ c/pointer_shibata/chap02/list02-06.c
@@ -0,0 +1,19 @@
+/* 
+   compare pointers
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int x[5];
+  int *p = x;
+
+  printf("p     ==  x    : %d\n", p     == x    );
+  printf("p     == &x[1] : %d\n", p     == &x[1]);
+  printf("&x[1] == &x[3] : %d\n", &x[1] == &x[3]);
+  printf("&x[2] != &x[3] : %d\n", &x[2] != &x[3]);
+  printf("&x[1] <  &x[2] : %d\n", &x[1] <  &x[2]);
+  printf("&x[2] <= &x[1] : %d\n", &x[2] <= &x[1]);
+
+  return (0);
+}
---
+++ c/pointer_shibata/chap02/list02-07.c
@@ -0,0 +1,16 @@
+/* 
+   pointer subtraction
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int x[5];
+  int *p = &x[4];
+
+  printf(" x   -  &x[1] : %d\n", (int)( x    - &x[1]));
+  printf("&x[2] - &x[0] : %d\n", (int)(&x[2] - &x[0]));
+  printf("&x[3] - p     : %d\n", (int)(&x[3] - p));
+
+  return (0);
+}
